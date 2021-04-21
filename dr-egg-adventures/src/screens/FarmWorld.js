78fd33bf8494cb8a98f2d9b398699d71b321bbfd
Michael Goodman <goodman.michael2011@gmail.com>
2021-04-21 12:09:13 +1000

    Revert "added world screen"
    This reverts commit f6e564910e621f15f82c6044a2b7701d745304b2.

--- dr-egg-adventures/src/screens/FarmWorld.js
+++ dr-egg-adventures/src/screens/FarmWorld.js
@@ -1,52 +1,9 @@
 import React from 'react'
-import techno from '../images/technoworld-dark.png';
-import farm from '../images/farmworld.png';
-import slum from '../images/slumworld-dark.png';
-import button from '../images/btn-Back.png';
-import worldText from '../images/world-text-flip.png';
-import exit from '../images/back-icon.png';
-import lab from '../images/lab-button.png';
-import { useHistory } from "react-router-dom";
 
 export const FarmWorld = () => {
-
-    const history = useHistory();
-
-    const homeNav = () => {
-        history.push('/map');
-    }
-
-    const handleLeave = () => {
-        history.push('/worlds');
-    }
-
-    const handleLabs = () => {
-        history.push('/lab');
-    }
-
     return (
         <div>
-            <div className = "world-image">
-                <img className = "technoworld" src = {techno} alt = "techno world" />
-                <img className = "farmworld" src = {farm} alt = "farm world" />
-                <img className = "slumworld" src = {slum} alt = "slum world" />  
-                      <div className = "hb-container">
-                         <img className = "home-button" src = {button} alt = "home button" onClick = {homeNav}/> 
-                      </div> 
-                      <div className = "farm-text-container">
-                        <img className = "world-text-farm" src = {worldText} alt = "text box"/>
-                        <div className = "farm-text">
-                            <h3 className = "world-title">FARMWORLD</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </div>
-                        <div className = "farm-exit" onClick = {handleLeave}>
-                            <img className = "back-icon" src = {exit} alt = "back icon"/>
-                        </div>
-                    </div>
-                    <div className = "lab-button">
-                        <img className = "lab-button" src = {lab} alt = "lab button" onClick = {handleLabs}/>
-                    </div>
-            </div>
+            
         </div>
     )
 }
--- dr-egg-adventures/src/screens/Worlds.js
+++ dr-egg-adventures/src/screens/Worlds.js
@@ -1,40 +1,9 @@
 import React from 'react'
-import techno from '../images/technoworld.png';
-import farm from '../images/farmworld.png';
-import slum from '../images/slumworld.png';
-import button from '../images/btn-Back.png';
-import { useHistory } from "react-router-dom";
 
 export const Worlds = () => {
-
-const history = useHistory();
-
-const homeNav = () => {
-    history.push('/map');
-}
-
-const technoNav = () => {
-    history.push('/TechnoWorld');
-}
-
-const farmNav = () => {
-    history.push('/FarmWorld');
-}
-
-const slumNav = () => {
-    history.push('/SlumWorld');
-}
-
     return (
         <div>
-            <div className = "world-image">
-                <img className = "technoworld" src = {techno} alt = "techno world" onClick = {technoNav}/>
-                <img className = "farmworld" src = {farm} alt = "farm world" onClick = {farmNav}/>
-                <img className = "slumworld" src = {slum} alt = "slum world" onClick = {slumNav}/>  
-                    <div className = "hb-container">
-                        <img className = "home-button" src = {button} alt = "home button" onClick = {homeNav}/> 
-                    </div>     
-            </div>
+            <h1> Worlds page </h1>            
         </div>
     )
 }
