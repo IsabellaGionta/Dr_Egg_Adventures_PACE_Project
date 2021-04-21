caa237dc80449fdc21c7b5744a46ed11d0cf64d1
Michael Goodman <goodman.michael2011@gmail.com>
2021-04-21 20:46:25 +1000

    added trailer and lab page

--- dr-egg-adventures/src/screens/Worlds.js
+++ dr-egg-adventures/src/screens/Worlds.js
@@ -3,6 +3,7 @@ import techno from '../images/technoworld.png';
 import farm from '../images/farmworld.png';
 import slum from '../images/slumworld.png';
 import button from '../images/btn-Back.png';
+import snake from '../images/Snake.png';
 import { useHistory } from "react-router-dom";
 
 export const Worlds = () => {
@@ -33,7 +34,10 @@ const slumNav = () => {
                 <img className = "slumworld" src = {slum} alt = "slum world" onClick = {slumNav}/>  
                     <div className = "hb-container">
                         <img className = "home-button" src = {button} alt = "home button" onClick = {homeNav}/> 
-                    </div>     
+                    </div> 
+                    <div className = "snake-container">
+                        <img className = "chatbot" src = {snake} alt = "chatbot" /> 
+                    </div>    
             </div>
         </div>
     )
