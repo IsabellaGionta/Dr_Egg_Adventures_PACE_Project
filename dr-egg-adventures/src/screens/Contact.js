f2a98d12b1e80aff07de960310e21c0d6b1d95de
Isabella gionta <isabella.gionta123@gmail.com>
2021-04-22 19:17:48 +1000

    Modifying styling

--- dr-egg-adventures/src/screens/Contact.js
+++ dr-egg-adventures/src/screens/Contact.js
@@ -21,73 +21,39 @@ export const Contact = () => {
         <div className="Contact-Background-Container">
           <div className="Contact-Container">
  
-          <Form className="form">
-            <h1 className="Title"> Dr Egg Adventures </h1>
-            <h2 className="text-center"> Contact Form </h2>
-            <div className="FormGroup">
-          <FormGroup>
-            <div className="label"> <Label> Full Name </Label> </div>
-              <Input  type="FullName" />
-            </FormGroup>
-            </div>
+            <Form className="Contact-Form">
+              <h1 className="Contact-Title"> Dr Egg Adventures </h1>
+              <h2 className="text-center"> Contact Form </h2>
+              <div className="Contact-FormGroup">
+                <FormGroup>
+                  <div className="Contact-Label"> <Label> Full Name </Label> </div>
+                    <Input  type="FullName" />
+                  </FormGroup>
+              </div>
+              <div className="Contact-FormGroup">
+                <FormGroup>
+                  <div className="Contact-Label"> <Label>Email</Label> </div>
+                    <Input  type="email" name="email" id="Email" />
+                </FormGroup>
+              </div> 
 
-            <div className="FormGroup">
-            <FormGroup>
-              <div className="label"> <Label>Email</Label> </div>
-              <Input  type="email" name="email" id="Email" />
-            </FormGroup>
-          </div> 
+              <div className="Contact-FormGroup">
+                <FormGroup>
+                  <div className="Contact-Label"><Label>Message</Label></div>
+                    <Input className="Contact-MessageBox" type="textarea" name="text" id="message" />
+                </FormGroup>
+              </div>
+                
+              <Button className="Contact-Button"><span className="text"> SUBMIT </span></Button>
+            </Form>
 
-          <div className="FormGroup">
-            <FormGroup>
-              <div className="label"><Label>Message</Label></div>
-              <Input className="messageBox" type="textarea" name="text" id="message" />
-            </FormGroup>
-            </div>
-            
-            <Button className="ContactButton"><span className="text"> SUBMIT </span></Button>
-          </Form>
-
-          <img className="back characters" src={Back} onClick={BackEventHandler} alt="Back"/>
-          <a href="/chatbot">
-           <img className="snake" src={Snake} alt="Snake"/>  
-           </a>          
-           </div>
-
+            <img className="Back-Button characters" src={Back} onClick={BackEventHandler} alt="Back"/>
+            <a href="/chatbot">
+             <img className="Chatbot-Snake" src={Snake} alt="Snake"/>  
+            </a>          
+          </div>
         </div>
-        
-
       );
     }
     
 export default Contact
-
-
-{/* <Container className="Contact-Container">
-<Row className="justify-content-md-center">
-  <Col xs={12} md={6}>
-
-<Form className="form">
-<Form.Group controlid="FullName">
-  <Form.Label> Full Name </Form.Label>
-  <Form.Control type="FullName" placeholder="Type Your Full Name Here"> </Form.Control>
-</Form.Group>
-
-<Form.Group controlid="Email">
-  <Form.Label>  Email </Form.Label>
-  <Form.Control type="Email" placeholder="Type Your Email Here"> </Form.Control>
-</Form.Group>
-
-<Form.Group controlid="Message">
-  <Form.Label> Message </Form.Label>
-  <Form.Control type="Message" placeholder="Type Your Message Here"> </Form.Control>
-</Form.Group>
-
-<Button>Submit</Button>
-
-</Form>
-</Col>
-</Row>
-<img className="back characters" src={Back} onClick={BackEventHandler} alt="Back"/>
-<img className="snake" src={Snake} alt="Snake"/>     
-</Container> */}
