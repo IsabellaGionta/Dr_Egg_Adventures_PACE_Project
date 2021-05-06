b39d4763f3c0ad6ff07a6fd895d9c9014a3d600d
LAPTOP-A4KB9SKJ\dxie1 <dxie16@outlook.com>
2021-05-06 16:08:41 +1000

    temporary fix to World Map

--- dr-egg-adventures/src/screens/Map.js
+++ dr-egg-adventures/src/screens/Map.js
@@ -5,11 +5,6 @@ import { useHistory } from "react-router-dom";
 import { Button } from 'react-bootstrap';
 import Worlds from '../images/Worlds.PNG';
 import ChatbotScreen from './ChatbotScreen.js'
-
- 
-
-
-
 import Snake from '../images/Snake.png';
 
 
@@ -28,26 +23,31 @@ export const Map = () => {
 
     return (
         <div className="Map-Container">
-          <img className="MapImage" src={map} alt="Map" useMap="#Map" />
+          <img className="MapImage" src={map} alt="Map" useMap="#Map"/>
+          <a href = "./Game">
           <div className="GamesHeading"> GAMES</div>
+          </a>
+          <a href = "./Resources">
           <div className="ResourcesHeading"> RESOURCES</div>
+          </a>
+          <a href = "./Characters">
           <div className="CharactersHeading"> CHARACTERS </div>
+          </a>
+          <a href = "./Constact">
           <div className="ContactHeading"> CONTACT </div>
+          </a>
+          <a href = "./Lab">
           <div className="LabHeading"> LAB </div>
+          </a>
+          <a href = "./Worlds">
           <div className="WorldsHeading"> WORLDS </div>
+          </a>
+          <a href = "./Trailer">
           <div className="TrailerHeading"> TRAILER </div>
+          </a>
+          <a href = "./online-store">
           <div className="OnlineStoreHeading"> ONLINE STORE </div>
-
-          <map name="Map" id="Map">
-            <area shape="poly" coords="901, 300, 1081, 530, 1085, 600, 665, 574, 901, 300 " href="/game" alt="Game" />
-            <area  shape="circle" coords="185, 700, 75 " href="/characters " alt="Characters" />
-            <area  shape="circle" coords="370, 810, 60  " href="/resources " alt="Resources" />
-            <area  shape="circle" coords="780, 700, 45 " href="/trailer" alt="Trailer" />
-            <area  shape="circle" coords="1750, 850, 70 " href="/contact " alt="Contact" />
-            <area  shape="poly" coords="1570, 650, 1400, 825, 1100, 825, 1100, 500, 1410, 510 " href="/worlds" alt="World" />
-            <area  shape="poly" coords="1450, 250, 1400, 505, 1100, 500, 1150, 200, 1410, 110 " href="/lab" alt="Lab" />
-            <area  shape="circle" coords="1000, 730, 65 " href="/online-store" alt="Online Store" />
-          </map>
+          </a>
  
            <a href="/chatbot">
            <img className="Map-Snake" src={Snake} alt="Snake"/>  
