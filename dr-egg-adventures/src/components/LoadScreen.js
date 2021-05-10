bd5b17c1a6bc1e184550897ce58ddd9183b605ee
husei <edin.huseinspahic@students.mq.edu.au>
2021-05-11 01:14:53 +1000

    loading screen done

---
+++ dr-egg-adventures/src/components/LoadScreen.js
@@ -0,0 +1,23 @@
+import { Spinner } from 'reactstrap';
+import styled from 'styled-components';
+
+const Overlay = styled.div`
+	position: fixed;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	background: rgba(0, 0, 0, 0.5);
+`;
+
+const LoadScreen = ({ visible }) => (
+	<>
+		{visible && (
+			<Overlay>
+				<Spinner />
+			</Overlay>
+		)}
+	</>
+);
+
+export default LoadScreen;
