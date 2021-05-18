5be983d60a4842bae8218713052f9d86b9cd707c
LAPTOP-A4KB9SKJ\dxie1 <dxie16@outlook.com>
2021-05-18 23:14:20 +1000

    Revert "Save before testing"
    This reverts commit de2797b16cd4583f64c635510966a13d7f067ba2.

--- dr-egg-adventures/src/screens/OnlineStore.js
+++ dr-egg-adventures/src/screens/OnlineStore.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Back from '../images/btn-Back.png';
 import Snake from '../images/Snake.png';
 import { Button} from 'reactstrap';
-import Book from '../images/Book.png';
+import Blackboard from '../images/Blackboard.png';
 import { useHistory } from "react-router-dom";
 
 
@@ -18,16 +18,18 @@ export const OnlineStore = () => {
       }
 
     return (
-
-      <div className="OnlineStore-Background-Container">
-        <img class="BookStore" src={Book} alt = "Book"/>
-        <Button className="OnlineStore-Button" onClick={() => 
-        { alert('Redirecting to Dr Egg Adventures Online Store Page!'); window.location.href = "https://dreggadventures.com/shop/?v=6cc98ba2045f";}} >ENTER OUR ONLINE STORE </Button>
-        <img className="Back-Button characters" src={Back} onClick={BackEventHandler} alt="Back"/>
-        <a href="/chatbot">
-          <img className="Chatbot-Snake" src={Snake} alt="Snake"/>  
-        </a>
-      </div>
+            <div className="OnlineStore-Background-Container">
+              <div className="OnlineStore-Container">
+                <img class="Blackboard" src={Blackboard} alt = "Blackboard"/>
+                <Button className="OnlineStore-Button" onClick={() => 
+                  { alert('Redirecting to Dr Egg Adventures Online Store Page!'); window.location.href = "https://dreggadventures.com/shop/?v=6cc98ba2045f";}} >ENTER OUR ONLINE STORE </Button>
+          
+                <img className="Back-Button characters" src={Back} onClick={BackEventHandler} alt="Back"/>
+                <a href="/chatbot">
+                  <img className="Chatbot-Snake" src={Snake} alt="Snake"/>  
+                </a>
+              </div>
+            </div>
     )
 }
 export default OnlineStore
