ca7a01fea9f4ff200b32a2f2d3aaa9eb6ee050c4
Michael Goodman <kristina.pavleski@students.mq.edu.au>
2021-05-18 19:00:40 +1000

    Added trailer, game and lab screens

--- dr-egg-adventures/src/screens/Game.js
+++ dr-egg-adventures/src/screens/Game.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import btnback from '../images/btn-Back.png';
+import btnbackglow from '../images/btn-Back-hover.png';
+import snake from '../images/Snake.png';
+import blackboard from '../images/Blackboard.png';
+import { useHistory } from "react-router-dom";
 
 export const Game = () => {
+
+    const history = useHistory();
+
+    const homeNav = () => {
+        history.push('/map');
+    }
+
     return (
-        <div>
-            <h1> Games Page</h1>
+        <div className = "Lab-Background-Container">
+            <div className = "back-container"> 
+                <img className = "Back-Button" 
+                src = {btnback}
+                onMouseOver = {e => e.currentTarget.src = btnbackglow}
+                onMouseOut={e => e.currentTarget.src = btnback}
+                alt = "home button" 
+                onClick = {homeNav}/>  
+            </div>
+            <div className = "snake-container">
+                <img className = "chatbot" src = {snake} alt = "chatbot" /> 
+            </div> 
+            <a href = "https://apps.apple.com/au/app/dr-egg-adventures-laboratory/id1372550003"><img className = "Blackboard" src = {blackboard} alt = "blackboard" /></a>
         </div>
     )
 }
