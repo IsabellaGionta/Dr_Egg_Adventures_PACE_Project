739dc8b1a8674ab657da07e0f8c765abbfa6487c
Kristina Pavleski <kristina.pavleski@students.mq.edu.au>
2021-05-19 22:05:10 +1000

    Merge branch 'Kristina_Develop' into develop

--- dr-egg-adventures/src/App.js
+++ dr-egg-adventures/src/App.js
@@ -1,100 +1,119 @@
 import './App.css';
-import './external.css'
+import './external.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Home from './screens/Home.js'
-import Map from './screens/Map.js'
-import Game from './screens/Game.js'
-import Contact from './screens/Contact.js'
-import Characters from './screens/Characters.js'
-import FarmWorld from './screens/FarmWorld.js'
-import SlumWorld from './screens/SlumWorld.js'
-import TechnoWorld from './screens/TechnoWorld.js'
-import Lab from './screens/Lab.js'
-import OnlineStore from './screens/OnlineStore.js'
-import Trailer from './screens/Trailer.js'
-import Resources from './screens/Resources.js'
-import IndividualCharacters from './screens/IndividualCharacters.js'
-import Worlds from './screens/Worlds.js'
-import ChatBotScreen from './screens/ChatbotScreen.js'
-import Loading from './screens/LoadingScreen.js'
+import Home from './screens/Home.js';
+import Map from './screens/Map.js';
+import Game from './screens/Game.js';
+import Contact from './screens/Contact.js';
+import Characters from './screens/Characters.js';
+import FarmWorld from './screens/FarmWorld.js';
+import SlumWorld from './screens/SlumWorld.js';
+import TechnoWorld from './screens/TechnoWorld.js';
+import Lab from './screens/Lab.js';
+import OnlineStore from './screens/OnlineStore.js';
+import Trailer from './screens/Trailer.js';
+import Resources from './screens/Resources.js';
+import IndividualCharacters from './screens/IndividualCharacters.js';
+import Worlds from './screens/Worlds.js';
+import ChatBotScreen from './screens/ChatbotScreen.js';
+import Loading from './screens/LoadingScreen.js';
+import ContactForm from './screens/ContactForm';
+import SlumWorldSingle from './screens/SlumWorldSingle';
+import TechnoWorldSingle from './screens/TechnoWorldSingle';
+import FarmWorldSingle from './screens/FarmWorldSingle';
+import LabWorldSingle from './screens/LabWorldSingle';
 import WeirdCreations from './screens/WeirdCreations.js'
 import CrazyFacts from './screens/CrazyFacts';
 import Fanfic from './screens/Fanfic';
 
-
 function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/'>
+					<Home />
+				</Route>
 
-  
-  return (
-    <Router>
-      <Switch>
+				<Route path='/map'>
+					<Map />
+				</Route>
 
-        <Route exact path="/">
-          <Home />
-        </Route>
+				<Route path='/game'>
+					<Game />
+				</Route>
 
-        <Route  path="/map">
-          <Map />
-        </Route>
+				<Route path='/contact'>
+					<Contact />
+				</Route>
 
-        <Route path="/game">
-          <Game />
-        </Route>
+				<Route path='/contact-form'>
+					<ContactForm />
+				</Route>
 
-        <Route path="/contact">
-          <Contact />
-        </Route>
+				<Route path='/characters'>
+					<Characters />
+				</Route>
 
-        <Route path="/characters">
-          <Characters />
-        </Route>
+				<Route path='/each-character'>
+					<IndividualCharacters />
+				</Route>
 
-        <Route path="/each-character">
-          <IndividualCharacters />
-        </Route>
+				<Route path='/worlds'>
+					<Worlds />
+				</Route>
 
-        
-        <Route path="/worlds">
-          <Worlds />
-        </Route>
+				<Route path='/farmWorld1'>
+					<FarmWorldSingle />
+				</Route>
 
-        <Route path="/farmWorld">
-          <FarmWorld />
-        </Route>
+				<Route path='/slumWorld1'>
+					<SlumWorldSingle />
+				</Route>
 
-        <Route path="/slumWorld">
-          <SlumWorld />
-        </Route>
+				<Route path='/technoWorld1'>
+					<TechnoWorldSingle />
+				</Route>
 
-        <Route path="/technoWorld">
-          <TechnoWorld />
-        </Route>
+				<Route path='/lab1'>
+					<LabWorldSingle />
+				</Route>
 
-        <Route path="/lab">
-          <Lab />
-        </Route>
+				<Route path='/farmWorld'>
+					<FarmWorld />
+				</Route>
 
-        <Route path="/online-store">
-          <OnlineStore />
-        </Route>
+				<Route path='/slumWorld'>
+					<SlumWorld />
+				</Route>
 
-        <Route path="/trailer">
-          <Trailer />
-        </Route>
+				<Route path='/technoWorld'>
+					<TechnoWorld />
+				</Route>
 
-        <Route path="/resources">
-          <Resources />
-        </Route>
+				<Route path='/lab'>
+					<Lab />
+				</Route>
 
-        
-        <Route path="/chatbot">
-          <ChatBotScreen />
-        </Route>
+				<Route path='/online-store'>
+					<OnlineStore />
+				</Route>
 
-        <Route path="/loading">
-          <Loading />
-        </Route>
+				<Route path='/trailer'>
+					<Trailer />
+				</Route>
+
+				<Route path='/resources'>
+					<Resources />
+				</Route>
+
+				<Route path='/chatbot'>
+					<ChatBotScreen />
+				</Route>
+
+				<Route path='/loading'>
+					<Loading />
+				</Route>
 
         <Route path="/weirdcreations">
           <WeirdCreations />
@@ -107,12 +126,10 @@ function App() {
         <Route path="/fanfiction">
           <Fanfic />
         </Route>
-      
-      </Switch>
-    </Router>
-
-    
-  );
+        
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
--- dr-egg-adventures/src/screens/Contact.js
+++ dr-egg-adventures/src/screens/Contact.js
@@ -1,24 +1,25 @@
-// import React, { useState } from 'react';
-// import {
-// 	Button,
-// 	Form,
-// 	FormGroup,
-// 	Label,
-// 	Input,
-// 	Modal,
-// 	ModalBody,
-// 	ModalHeader,
-// 	ModalFooter,
-// } from 'reactstrap';
-// import Snake from '../images/Snake.png';
-// import Back from '../images/btn-Back.png';
-// import Blackboard from '../images/Blackboard.png';
-// import { useHistory } from 'react-router-dom';
-// import axios from '../axios';
-// import '../assets/css/modal.css';
-// import '../assets/css/spinner.css';
-// import LoadScreen from '../components/LoadScreen';
-// import styled from 'styled-components';
+import React, { useState } from 'react';
+import {
+	Button,
+	Form,
+	FormGroup,
+	Label,
+	Input,
+	Modal,
+	ModalBody,
+	ModalHeader,
+	ModalFooter,
+} from 'reactstrap';
+import Snake from '../images/Snake.png';
+import Back from '../images/btn-Back.png';
+import Blackboard from '../images/Blackboard.png';
+import { useHistory } from 'react-router-dom';
+import axios from '../axios';
+import '../assets/css/modal.css';
+import '../assets/css/spinner.css';
+import LoadScreen from '../components/LoadScreen';
+import styled from 'styled-components';
+import ThemeButton from '../components/Button';
 
 // const ErrorMsg = styled.span`
 // 	display: block;
@@ -109,149 +110,79 @@
 // 		}
 // 	};
 
-// 	return (
-// 		<>
-// 			<div className='Contact-Background-Container'>
-// 				<div className='Contact-Container'>
-// 					<img
-// 						className='Blackboard'
-// 						src={Blackboard}
-// 						alt='Blackboard'></img>
-// 					{/* <h1 className="Contact-Title"> Dr Egg Adventures </h1> */}
+	return (
+		<>
+			<div className='Contact-Background-Container'>
+				<div className='Contact-Container'>
+					<img
+						className='Blackboard'
+						src={Blackboard}
+						alt='Blackboard'></img>
+					<h1 className='Contact-Title'> Contact us </h1>
+					<div className='Contact-Info'>
+						<p> MOBILE NUMBER: +61 0415 442 209 </p>
+						<p> PHONE NUMBER: +61 02 9314 5121</p>
+						<p>
+							{' '}
+							ADDRESS: Dr Egg Digital - The Dr Egg Adventures
+							Project{' '}
+						</p>
+						<p className='indent'>
+							{' '}
+							City of Sydney Cultural Precint Office{' '}
+						</p>
+						<p className='indent'>
+							{' '}
+							Suite, 1, Level 22, 66 Oxford Street{' '}
+						</p>
+						<p className='indent'>
+							{' '}
+							East Sydney, NSW, Australia, 2010{' '}
+						</p>
+						<p>
+							{' '}
+							WEBSITE:{' '}
+							<a href='www.dreggadventures.com'>
+								{' '}
+								www.dreggadventures.com{' '}
+							</a>{' '}
+						</p>
+					</div>
+					<ThemeButton
+						fontSize={5}
+						top={65}
+						onClick={() => history.push('/contact-form')}>
+						Form
+					</ThemeButton>
+					<img
+						className='Back-Button characters'
+						src={Back}
+						onClick={BackEventHandler}
+						alt='Back'
+					/>
+					<a href='/chatbot'>
+						<img
+							className='Chatbot-Snake'
+							src={Snake}
+							alt='Snake'
+						/>
+					</a>
+				</div>
+			</div>
+			<LoadScreen visible={loading} />
+			<Modal isOpen={modal} toggle={toggleModal}>
+				<ModalHeader toggle={toggleModal}>
+					{response.status ? 'Oops!' : 'Thank you!'}{' '}
+				</ModalHeader>
+				<ModalBody>{response.msg}</ModalBody>
+				<ModalFooter>
+					<Button color='primary' onClick={toggleModal}>
+						Confirm
+					</Button>
+				</ModalFooter>
+			</Modal>
+		</>
+	);
 
-// 					<Form className='Contact-Form' onSubmit={handleSubmit}>
-// 						<h2 className='Contact-Title'> Contact Form </h2>
 
-// 						{/* <h1 className="Contact-Title"> Dr Egg Adventures </h1> */}
-// 						<div className='Contact-FormGroup'>
-// 							<FormGroup>
-// 								<div className='Contact-Label'>
-// 									{' '}
-// 									<Label> Full Name </Label>{' '}
-// 								</div>
-// 								<Input
-// 									type='FullName'
-// 									required
-// 									value={form['name']}
-// 									onChange={e => handleFieldChange(e, 'name')}
-// 								/>
-// 								{errors && errors.name && (
-// 									<ErrorMsg>{errors.name}</ErrorMsg>
-// 								)}
-// 							</FormGroup>
-// 						</div>
-// 						<div className='Contact-FormGroup'>
-// 							<FormGroup>
-// 								<div className='Contact-Label'>
-// 									{' '}
-// 									<Label>Email</Label>{' '}
-// 								</div>
-// 								<Input
-// 									type='email'
-// 									name='email'
-// 									id='Email'
-// 									required
-// 									value={form['email']}
-// 									onChange={e =>
-// 										handleFieldChange(e, 'email')
-// 									}
-// 								/>
-// 								{errors && errors.email && (
-// 									<ErrorMsg>{errors.email}</ErrorMsg>
-// 								)}
-// 							</FormGroup>
-// 						</div>
-
-// 						<div className='Contact-FormGroup'>
-// 							<FormGroup>
-// 								<div className='Contact-Label'>
-// 									<Label>Message</Label>
-// 								</div>
-// 								<Input
-// 									className='Contact-MessageBox'
-// 									type='textarea'
-// 									name='text'
-// 									id='message'
-// 									required
-// 									value={form['message']}
-// 									onChange={e =>
-// 										handleFieldChange(e, 'message')
-// 									}
-// 								/>
-// 								{errors && errors.message && (
-// 									<ErrorMsg>{errors.message}</ErrorMsg>
-// 								)}
-// 							</FormGroup>
-// 						</div>
-
-// 						<Button
-// 							className='Contact-Button'
-// 							disabled={
-// 								Object.keys(errors).length ? true : false
-// 							}>
-// 							<span className='text'> SUBMIT </span>
-// 						</Button>
-// 					</Form>
-
-// 					<div className='Contact-Info'>
-// 						<p> MOBILE NUMBER: +61 0415 442 209 </p>
-// 						<p> PHONE NUMBER: +61 02 9314 5121</p>
-// 						<p>
-// 							{' '}
-// 							ADDRESS: Dr Egg Digital - The Dr Egg Adventures
-// 							Project{' '}
-// 						</p>
-// 						<p className='indent'>
-// 							{' '}
-// 							City of Sydney Cultural Precint Office{' '}
-// 						</p>
-// 						<p className='indent'>
-// 							{' '}
-// 							Suite, 1, Level 22, 66 Oxford Street{' '}
-// 						</p>
-// 						<p className='indent'>
-// 							{' '}
-// 							East Sydney, NSW, Australia, 2010{' '}
-// 						</p>
-// 						<p>
-// 							{' '}
-// 							WEBSITE:{' '}
-// 							<a href='www.dreggadventures.com'>
-// 								{' '}
-// 								www.dreggadventures.com{' '}
-// 							</a>{' '}
-// 						</p>
-// 					</div>
-
-// 					<img
-// 						className='Back-Button characters'
-// 						src={Back}
-// 						onClick={BackEventHandler}
-// 						alt='Back'
-// 					/>
-// 					<a href='/chatbot'>
-// 						<img
-// 							className='Chatbot-Snake'
-// 							src={Snake}
-// 							alt='Snake'
-// 						/>
-// 					</a>
-// 				</div>
-// 			</div>
-// 			<LoadScreen visible={loading} />
-// 			<Modal isOpen={modal} toggle={toggleModal}>
-// 				<ModalHeader toggle={toggleModal}>
-// 					{response.status ? 'Oops!' : 'Thank you!'}{' '}
-// 				</ModalHeader>
-// 				<ModalBody>{response.msg}</ModalBody>
-// 				<ModalFooter>
-// 					<Button color='primary' onClick={toggleModal}>
-// 						Confirm
-// 					</Button>
-// 				</ModalFooter>
-// 			</Modal>
-// 		</>
-// 	);
-// };
-
-// export default Contact;
+ export default Contact;
