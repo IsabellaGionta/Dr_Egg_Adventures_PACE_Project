ffc635abf6fddb545b09652f0f673f302b4654eb
Kristina Pavleski <kristina.pavleski@students.mq.edu.au>
2021-05-30 20:02:53 +1000

    game page updates

--- dr-egg-adventures/src/screens/Game.js
+++ dr-egg-adventures/src/screens/Game.js
@@ -1,37 +1,41 @@
 import React from 'react'
-import btnback from '../images/btn-Back.png';
-import btnbackglow from '../images/btn-Back-hover.png';
-import snake from '../images/characters/Snake.png';
-import background from '../images/popup-bg.png';
 import { useHistory } from "react-router-dom";
+import Popup from '../components/Popup';
+import { useState } from 'react';
+import ardash from '../images/ardash-face.png';
+import vivi from '../images/vivi-face.png';
+import google from '../images/google-play.png';
+import apple from '../images/app-store.png';
 
 export const Game = () => {
 
     const history = useHistory();
 
-    const homeNav = () => {
+    const [buttonPopup, setButtonPopup] = useState(true);
+
+    if (buttonPopup == false) {
         history.push('/map');
     }
 
     return (
-        <div className = "Lab-Background-Container">
+        <div className = "Game-Background-Container">
             
-            <div className = "game-container">
-                <img className = "game-bg" src = {background} alt = "background" />
-            </div>
-
-            <div className = "back-container"> 
-                <img className = "Back-Button" 
-                src = {btnback}
-                onMouseOver = {e => e.currentTarget.src = btnbackglow}
-                onMouseOut={e => e.currentTarget.src = btnback}
-                alt = "home button" 
-                onClick = {homeNav}/>  
-            </div>
-            <div className = "snake-container">
-                <img className = "chatbot" src = {snake} alt = "chatbot" /> 
-                
-            </div> 
+            <Popup trigger={buttonPopup} setTrigger = {setButtonPopup}>
+                <div className = "game-container">
+                    <h1 className = "game-text1">DR EGG ADVENTURES INTERACTIVE 
+                    <br></br>
+                    BOOK AND PUZZLE GAME</h1> 
+                    <div className = "gamebtn">
+                        <img className="game-button" src={google} alt = "google play"/>
+                        <a className = "game-link" href="https://apps.apple.com/au/app/dr-egg-adventures-laboratory/id1372550003">
+                            <img className="apple-button" src={apple} alt = "apple"/>
+                        </a>
+                    </div>
+                    <p className= "game-text2">Click to download the app now!</p> 
+                </div>
+                <img className="ardash-face" src={ardash} alt = "ardash" />
+                <img className="vivi-face" src={vivi} alt = "vivi"/>
+            </Popup>
             
 
         </div>
