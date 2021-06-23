03fdec7d55111f59735e355d07ac5d77ee81d60b
kristina-pav <64387652+kristina-pav@users.noreply.github.com>
2021-06-23 19:54:54 +1000

    final media queries done

--- dr-egg-adventures/src/App.js
+++ dr-egg-adventures/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import './Mobile.css';
 import './Largescreen.css';
+import './Mobile.css';
 import './external.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
