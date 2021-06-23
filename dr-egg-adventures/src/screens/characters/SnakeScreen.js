f65c4e1eea9046ed5cc50c82233cc56fb268d389
IsabellaGionta <43375786+IsabellaGionta@users.noreply.github.com>
2021-06-23 18:39:25 +1000

    cleaning up code

--- dr-egg-adventures/src/screens/characters/SnakeScreen.js
+++ dr-egg-adventures/src/screens/characters/SnakeScreen.js
@@ -73,12 +73,6 @@ export const SnakeScreen = () => {
 
         </div>
 
-        {/* <LandscapePopup trigger={modalState2} setTrigger = {setModalState2}>
-          <div className = "popup-background">
-            <p className="Landscape-text"> For a better viewing experience, turn device to Landscape view. </p>
-          </div>
-        </LandscapePopup> */}
-
         
     
       </div>
